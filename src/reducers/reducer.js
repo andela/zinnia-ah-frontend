1638e5ec43bd2770b2415d70ce3653378e5b2bc2
Oparah Dimkpa <Oparahdc@gmail.com>
2019-04-29 12:48:52 +0100

    [Chore 164797099] Setup redux

---
+++ src/reducers/reducer.js
@@ -0,0 +1,10 @@
+const DEFAULT_STATE = {};
+
+const rootReducer = (state = DEFAULT_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default rootReducer;
