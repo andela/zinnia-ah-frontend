650d64c4a4816dff657f72a8235739639516d7c5
Oparah Dimkpa <Oparahdc@gmail.com>
2019-04-29 13:40:10 +0100

    [Chore 164797099] Setup jest testing configuration

---
+++ src/tests/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../components/App';
+
+describe('App', () => {
+  it('should render correctly', () => {
+    const component = shallow(<App />);
+
+    expect(component).toMatchSnapshot();
+    expect(component.html()).toEqual(
+      '<div><h1 class="toothy"> My React App!! </h1></div>',
+    );
+  });
+});
