ac998f152297997731aa5dda96d3edd5fc6c57c9
Oparah Dimkpa <Oparahdc@gmail.com>
2019-04-29 14:45:51 +0100

    [chore 164797105] setup sass processing

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import App from './components/App';
+import App from './components/App.jsx';
 
 const Routes = () => <App />;
 
