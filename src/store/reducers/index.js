2bd11b0c446b48cf41773d12830eec0c91496814
Favour Afolayan <favourafolayan@gmail.com>
2019-05-03 06:42:09 +0100

    [chore] setup actions and reducer for signup

---
+++ src/store/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+import signup from './authReducer';
+
+export default combineReducers({ signup });
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
+        test: /\.(png|jp(e*)g|svg|gif|ttf|woff|woff2|eot)$/,
         use: [
           {
             loader: 'file-loader',
