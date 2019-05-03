b0580781d15a305592587b4025630cae74a3527e
eben-k <ennim.consults@gmail.com>
2019-05-03 16:00:08 +0100

    [feature] add other field valdations

--- src/utils/formValidator.js
+++ src/utils/formValidator.js
@@ -7,5 +7,27 @@ export default function validator(userCredentials) {
       validationErrors.push(`${key} cannot be empty`);
     }
   });
+
+  if (userCredentials['username'].trim().length < 8) {
+    validationErrors.push('username must be at least 8 characters');
+  }
+
+  if (userCredentials['password'].trim().length < 8) {
+    validationErrors.push('password must be at least 8 characters');
+  }
+
+  if (!userCredentials['password'].match(/^([a-zA-Z0-9]+)$/)) {
+    validationErrors.push('password can only contain alphanumerics');
+  }
+
+  if (
+    !userCredentials['email']
+      .trim()
+      .match(
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+      )
+  ) {
+    validationErrors.push('email must be a valid email');
+  }
   return validationErrors;
 }
