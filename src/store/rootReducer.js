143c0a3d3891ce11cc465fdb82fea7ea0d66f47e
nedyudombat <nedyudombat@gmail.com>
2019-05-13 15:24:02 +0100

    [feature #164797112] Setup folder structure

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import { authReducer } from './modules/auth';
 import { profileReducer } from './modules/profile';
-import { passwordReducer } from './modules/password';
 import { articleReducer } from './modules/article';
 
 export default combineReducers({
   auth: authReducer,
   profile: profileReducer,
-  password: passwordReducer,
   article: articleReducer,
 });
