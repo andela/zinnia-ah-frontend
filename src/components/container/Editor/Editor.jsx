abff80701178dfc627b53b463a7037b09ed2e224
Oparah Dimkpa <Oparahdc@gmail.com>
2019-05-16 21:29:26 +0100

    [Feature 164797112] Added tag functionality

--- src/components/container/Editor/Editor.jsx
+++ src/components/container/Editor/Editor.jsx
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Editor } from 'react-draft-wysiwyg';
 import draftToHtml from 'draftjs-to-html';
+import { WithContext as ReactTags } from 'react-tag-input';
 import '../../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './Editor.scss';
 
@@ -14,8 +15,8 @@ import { createArticle } from '../../../store/modules/article';
 
 //Components
 import Button from '../../presentationals/Button/Button';
-import Input from '../../presentationals/Input/Input';
 import Loader from '../../presentationals/Loader/Loader';
+import TextArea from '../../presentationals/TextArea/TextArea';
 
 export class EditorContainer extends Component {
   state = {
@@ -23,6 +24,29 @@ export class EditorContainer extends Component {
     description: '',
     body: EditorState.createEmpty(),
     imageThumbnail: '',
+    tags: [],
+  };
+
+  handleDelete = i => {
+    const { tags } = this.state;
+    this.setState({
+      tags: tags.filter((tag, index) => index !== i),
+    });
+  };
+
+  handleAddition = tag => {
+    this.setState(state => ({ tags: [...state.tags, tag] }));
+  };
+
+  handleDrag = (tag, currPos, newPos) => {
+    const tags = [...this.state.tags];
+    const newTags = tags.slice();
+
+    newTags.splice(currPos, 1);
+    newTags.splice(newPos, 0, tag);
+
+    // re-render
+    this.setState({ tags: newTags });
   };
 
   onEditorStateChange = body => {
@@ -37,6 +61,9 @@ export class EditorContainer extends Component {
       [event.target.name]: event.target.value,
     });
   };
+
+  onChange = () => {};
+  tags = () => {};
 
   fileUploadHandler = async file => {
     const { imageThumbnail } = this.state;
@@ -56,17 +83,16 @@ export class EditorContainer extends Component {
         imageThumbnail: data.data.secure_url,
       });
     }
-
     return data;
   };
   submitArticle = event => {
     event.preventDefault();
 
     const getDescription = string => {
-      return string.substring(0, 400);
+      return string.substring(0, 500);
     };
 
-    const { title, body, imageThumbnail } = this.state;
+    const { title, body, imageThumbnail, tags } = this.state;
     const description = `${getDescription(
       JSON.stringify(body.getCurrentContent().getPlainText()),
     )}...`;
@@ -76,6 +102,7 @@ export class EditorContainer extends Component {
       description,
       imageThumbnail,
       body: draftToHtml(convertToRaw(body.getCurrentContent())),
+      tags,
     };
     this.props.createArticle(articleInput);
   };
@@ -84,15 +111,23 @@ export class EditorContainer extends Component {
     const {
       article: { isLoading },
     } = this.props;
-    const { body } = this.state;
+
+    const { body, tags } = this.state;
+
+    const KeyCodes = {
+      comma: 188,
+      enter: 13,
+    };
+
+    const delimiters = [KeyCodes.comma, KeyCodes.enter];
+
     return (
       <div>
         {isLoading && <Loader text="please wait" size="large" />}
         <Navbar />
-        <Input
+        <TextArea
           placeholder="Title"
           className="dark"
-          type="text"
           onChange={this.inputHandler}
           value={this.state.title}
           name="title"
@@ -136,12 +171,23 @@ export class EditorContainer extends Component {
               },
             }}
           />
+
           <Button
             type="submit"
             value="POST ARTICLE"
             className="btn-dark"
             onClick={this.submitArticle}
           />
+          <div>
+            <ReactTags
+              tags={tags}
+              // suggestions={suggestions}
+              handleDelete={this.handleDelete}
+              handleAddition={this.handleAddition}
+              handleDrag={this.handleDrag}
+              delimiters={delimiters}
+            />
+          </div>
         </div>
       </div>
     );
