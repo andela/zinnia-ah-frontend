29c810efcda52e05e3021a42c7077996cc9d41ee
Oparah Dimkpa <Oparahdc@gmail.com>
2019-05-16 15:46:51 +0100

    [Feature 164797112] Added image uplaod functionality

--- src/components/container/Editor/Editor.jsx
+++ src/components/container/Editor/Editor.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { EditorState, convertToRaw } from 'draft-js';
 import Navbar from '../../presentationals/Navbar/Navbar';
 import { connect } from 'react-redux';
@@ -11,6 +12,7 @@ import './Editor.scss';
 //Reducer
 import { createArticle } from '../../../store/modules/article';
 
+//Components
 import Button from '../../presentationals/Button/Button';
 import Input from '../../presentationals/Input/Input';
 import Loader from '../../presentationals/Loader/Loader';
@@ -36,20 +38,42 @@ export class EditorContainer extends Component {
     });
   };
 
+  fileUploadHandler = async file => {
+    const { imageThumbnail } = this.state;
+    const url = 'https://api.cloudinary.com/v1_1/trix/image/upload';
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'zinniahteam');
+
+    const data = await axios.post(url, formData, {
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    });
+
+    if (imageThumbnail === '') {
+      this.setState({
+        imageThumbnail: data.data.secure_url,
+      });
+    }
+
+    return data;
+  };
   submitArticle = event => {
     event.preventDefault();
 
     const getDescription = string => {
-      return string.substring(0, 600);
+      return string.substring(0, 400);
     };
 
     const { title, body, imageThumbnail } = this.state;
+    const description = `${getDescription(
+      JSON.stringify(body.getCurrentContent().getPlainText()),
+    )}...`;
 
     const articleInput = {
       title,
-      description: getDescription(
-        draftToHtml(convertToRaw(body.getCurrentContent())),
-      ),
+      description,
       imageThumbnail,
       body: draftToHtml(convertToRaw(body.getCurrentContent())),
     };
@@ -73,14 +97,6 @@ export class EditorContainer extends Component {
           value={this.state.title}
           name="title"
         />
-        {/* <Input
-          placeholder="Description"
-          className="dark"
-          type="text"
-          onChange={this.inputHandler}
-          value={this.state.description}
-          name="description"
-        /> */}
         <div className="editor-container">
           <Editor
             toolbarOnFocus
@@ -89,6 +105,36 @@ export class EditorContainer extends Component {
             wrapperClassName="demo-wrapper"
             editorClassName="demo-editor"
             onEditorStateChange={this.onEditorStateChange}
+            toolbar={{
+              options: [
+                'inline',
+                'blockType',
+                'textAlign',
+                'colorPicker',
+                'link',
+                'emoji',
+                'image',
+                'history',
+              ],
+              inline: { inDropdown: true },
+              list: { inDropdown: true },
+              textAlign: { inDropdown: true },
+              link: { inDropdown: true },
+              history: { inDropdown: true },
+              image: {
+                uploadCallback: this.fileUploadHandler,
+                placeHolder: 'Write your story...',
+                previewImage: true,
+                inputAccept:
+                  'image/gif,image/jpeg,image/jpg,image/png,image/svg',
+                alignmentEnabled: false,
+                alt: { present: false, mandatory: false },
+                defaultSize: {
+                  height: '500',
+                  width: '1000',
+                },
+              },
+            }}
           />
           <Button
             type="submit"
