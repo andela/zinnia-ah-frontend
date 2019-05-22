275f8f21436ae7fe2dfab687ae2a2003e02c0252
Emmanuel Daniel <emmanuel.daniel@andela.com>
2019-05-22 13:07:45 +0100

    fixed

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { getToken } from './helpers';
+import { HOST_URL } from '../config/config';
 
 export const http = axios.create({
-  baseURL: process.env.HOST_URL,
+  baseURL: HOST_URL,
   headers: {
     Authorization: getToken(),
   },
